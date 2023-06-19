a712fce3a1fa202a3d58595a4c4c2c82db9361ad
DJ-Rhodes <damianjason.rhodes@gmail.com>
2023-06-19 10:52:42 +0200

    Initial commit: Spring Web setup

---
+++ src/main/java/com/djrhodes/boardgamenexus/BoardGameNexusApplication.java
@@ -0,0 +1,13 @@
+package com.djrhodes.boardgamenexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djrhodes/boardgamenexus/BoardGameNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djrhodes.boardgamenexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
